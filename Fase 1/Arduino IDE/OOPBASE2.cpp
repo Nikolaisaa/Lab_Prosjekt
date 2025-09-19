8fc960e02e82ca7439a64b89b0600ae0c65afbca
Nikolus Kambale Muheko <101116357+MuhekoNikolas@users.noreply.github.com>
2025-09-19 13:38:07 +0200

    Create OOPBASE2.cpp

---
+++ Fase 1/Arduino IDE/OOPBASE2.cpp
@@ -0,0 +1,78 @@
+#include <QTRSensors.h>
+
+class SensorObj {
+private:
+    int sensorCount_;
+    uint8_t* sensorPins_;
+    uint16_t* sensorThresholds_;
+    uint16_t* sensorValues_;
+
+public:
+    QTRSensors qtr;
+
+    SensorObj(int sensorCount, uint8_t* sensorPins, uint16_t* sensorThresholds) {
+        this->sensorCount_ = sensorCount;
+        this->sensorPins_ = new uint8_t[sensorCount];
+        this->sensorValues_ = new uint16_t[sensorCount];
+        this->sensorThresholds_ = new uint16_t[sensorCount];
+
+        for (int i = 0; i < sensorCount; i++) {
+            this->sensorPins_[i] = sensorPins[i];
+            this->sensorThresholds_[i] = sensorThresholds[i];
+            this->sensorValues_[i] = 0;
+        }
+    }
+
+    void readArray() {
+        qtr.read(sensorValues_);
+        
+        for (int i = 0; i < sensorCount_; i++) {
+            Serial.print(sensorValues_[i]);
+            Serial.print('\t');
+        }
+        Serial.print('\t');
+
+        for (int i = 0; i < sensorCount_; i++) {
+            if (sensorValues_[i] > sensorThresholds_[i]) {
+                Serial.print("X");
+            } else {
+                Serial.print("-");
+            }
+        }
+        Serial.println();
+    }
+
+    int getSensorCount() const {
+        return sensorCount_;
+    }
+
+    uint8_t* getSensorPins() const {
+        return sensorPins_;
+    }
+
+    ~SensorObj() {
+        delete[] sensorPins_;
+        delete[] sensorValues_;
+        delete[] sensorThresholds_;
+    }
+};
+
+const int sensorCount = 1;
+uint8_t sensorPins[] = {1};
+uint16_t sensorThresholds[] = {500};
+
+SensorObj so(sensorCount, sensorPins, sensorThresholds);
+
+void setup() {
+    Serial.begin(9600);
+
+    so.qtr.setTypeRC();
+    so.qtr.setSensorPins(so.getSensorPins(), so.getSensorCount());
+//
+//    delay(500);
+}
+
+void loop() {
+    so.readArray();
+    delay(50);
+}
